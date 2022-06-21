6de121a1ad6e395c43553d9c0781adb4775453cd
Tsu <34934915+msumari@users.noreply.github.com>
2022-06-21 20:40:47 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1HSAiKIcR10V69-N7QGxPabHitYnx8YE",
-  authDomain: "qr-code-26ff1.firebaseapp.com",
-  projectId: "qr-code-26ff1",
-  storageBucket: "qr-code-26ff1.appspot.com",
-  messagingSenderId: "601289054808",
-  appId: "1:601289054808:web:a61b80daca8bf2fbf3c82d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
